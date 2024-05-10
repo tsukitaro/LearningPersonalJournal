ebffbc707bd5c213422baa82df53dc73f55d6f4c
kert-daiki <dylax01@gmail.com>
2024-05-09 21:50:48 -0500

    add files

---
+++ aluraOne/java/Java-1/Java1_DesafioExtra.java
@@ -0,0 +1,5 @@
+public class Java1_DesafioExtra {
+  public static void main(String[] args) {
+     
+  }
+}
---
+++ aluraOne/java/Java-1/Java1_DesafioFinal.java
@@ -0,0 +1,19 @@
+import java.util.StringJoiner;
+import java.util.function.DoubleConsumer;
+
+/**
+ * Java1_DesafioFinal
+ */
+public class Java1_DesafioFinal {
+
+  public static void main(String[] args) {
+    String nombre = "Hiro Nakamura";
+    String tipoDeCuenta = "Corriente";
+    Double saldo = 1599.99;
+
+    System.out.println("***************************");
+    System.out.println("\n Nombre del cliente: " + nombre);
+    System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+    System.out.println("Saldo disponible: " + saldo);
+  }
+}
