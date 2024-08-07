0e512227a12801ec12e00a8ed2057c19f25c2826
kert-daiki <dylax01@gmail.com>
2024-08-07 11:54:11 -0500

    conditional statement - repeat topic whether a number is positive ...

--- Hyperskill/Simple Chat Bot with Java/Topics/Conditional statement/Determining whether a number is positive, negative, or zero/src/Main.java
+++ Hyperskill/Simple Chat Bot with Java/Topics/Conditional statement/Determining whether a number is positive, negative, or zero/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         int number = scanner.nextInt();
 
         // Your code here
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
 
         scanner.close();
     }
